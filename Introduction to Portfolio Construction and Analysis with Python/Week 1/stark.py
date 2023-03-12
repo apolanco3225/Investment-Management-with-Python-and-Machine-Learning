1ba69e5c3c8d99007c490f7b8a9e89d435264323
Arturo Polanco Lozano <apolanco3225@gmail.com>
2023-03-12 17:53:52 -0500

    update data dirs in all exercises

--- Introduction to Portfolio Construction and Analysis with Python/Week 1/stark.py
+++ Introduction to Portfolio Construction and Analysis with Python/Week 1/stark.py
@@ -8,7 +8,7 @@ def get_ffme_returns():
     """
     # load data
     returns = pd.read_csv(
-        "data/Portfolios_Formed_on_ME_monthly_EW.csv",
+        "../data/Portfolios_Formed_on_ME_monthly_EW.csv",
         header = 0, 
         index_col = 0, 
         na_values = -99.99, 
