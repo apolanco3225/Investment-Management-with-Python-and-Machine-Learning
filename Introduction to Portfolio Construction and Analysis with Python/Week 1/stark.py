3ff9b1a7a030894edcb021290eff43b0e9485c2a
Arturo Polanco Lozano <apolanco3225@gmail.com>
2023-03-13 12:21:52 -0500

    include var and cvar functions

--- Introduction to Portfolio Construction and Analysis with Python/Week 1/stark.py
+++ Introduction to Portfolio Construction and Analysis with Python/Week 1/stark.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import scipy.stats
+from scipy.stats import norm
+
 
 # data convinience functions
 def get_ffme_returns():
@@ -105,3 +108,68 @@ def is_normal(returns_data, level=0.01):
     statistic, p_value = scipy.stats.jarque_bera(returns_data)
     return p_value > level
     
+    
+def calculate_semideviation(returns_data):
+    """
+    Returns the semideviation aka negative semideviation
+    of the input returns. The returns can be either a Series
+    or a DataFrame
+    """
+    negative_values_mask = returns_data < 0
+    return returns_data[negative_values_mask].std(ddof=0)
+
+def calculate_var_historic(returns_data, level=5):
+    """
+    Returns the historic value at risk at a specific level.
+    i.e. returns the number such that "level" percent of the returns
+    fall below that number, and the (100-level) percent are above.
+    """
+    if isinstance(returns_data, pd.DataFrame):
+        return returns_data.aggregate(
+            calculate_var_historic, 
+            level = level
+        )
+    elif isinstance(returns_data, pd.Series):
+        return - np.percentile(
+            a = returns_data, 
+            q = level
+        )
+    else:
+        raise TypeError ("Expected returns to be a pandas Series or DataFrame")
+        
+
+def calculate_var_gaussian(returns_data, level = 5, modified=False):
+    """
+    Returns the Parametric Gaussian VaR of a pandas Series
+    or a DataFrame. 
+    If "modified" is True, then the modified VaR is returned
+    using the Cornish-Fisher modification. 
+    """
+    z_score = norm.ppf(level / 100)
+    
+    if modified:
+        # modify the z score based on observed skewness and kurtosis
+        s = calculate_skewness(returns_data)
+        k = calculate_kurtosis(returns_data)
+        z_score = (
+            z_score + 
+            (z_score ** 2 - 1) * s / 6 +
+            (z_score ** 3 - 3 * z_score) * (k - 3) / 24 -
+            (2 * z_score ** 3 - 5 * z_score) * (s ** 2) / 36
+        )
+    return -(returns_data.mean() + z_score * returns_data.std(ddof=0))
+
+
+def calculate_cvar_historic(returns_data, level=5):
+    """
+    Computes the conditional var of a Series or a DataFrame
+    """
+    if isinstance(returns_data, pd.Series):
+        is_beyond = r <= calculate_var_historic(returns_data, level=level)
+        return -returns_data[is_beyond].mean()
+    
+    elif isinstance(returns_data, pd.DataFrame):
+        return returns_data.aggregate(calculate_cvar_historic, level=level)
+    
+    else:
+        raise TypeError ("Expected returns to be a pandas Series or DataFrame")
