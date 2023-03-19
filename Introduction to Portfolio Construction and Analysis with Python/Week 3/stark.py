0da236e8074e17f1854f644ce88b355b4f1d0021
Arturo Polanco Lozano <apolanco3225@gmail.com>
2023-03-18 20:01:43 -0500

    include brownian motion function

--- Introduction to Portfolio Construction and Analysis with Python/Week 3/stark.py
+++ Introduction to Portfolio Construction and Analysis with Python/Week 3/stark.py
@@ -637,3 +637,25 @@ def summary_stats(returns_data, risk_free_rate=0.03):
     
     summary_df = pd.DataFrame(summary_dict)
     return summary_df
+
+
+def geometric_brownian_motion(
+    n_years = 10, 
+    n_scenarios = 1_000, 
+    mu=0.07, 
+    sigma = 0.15,
+    steps_per_year = 12, 
+    s_0 = 100.0
+):
+    """
+    Evolution of a stock price using geometric brownian motion model
+    """
+    delta_time = 1 / steps_per_year
+    n_steps = int(n_years * steps_per_year)
+    rets_plus_one = np.random.normal(
+        loc = (1 + mu * delta_time), 
+        scale = (sigma * np.sqrt(delta_time)), 
+        size=(n_steps, n_scenarios)
+    )
+    prices = s_0 * pd.DataFrame(rets_plus_one).cumprod()
+    return prices
