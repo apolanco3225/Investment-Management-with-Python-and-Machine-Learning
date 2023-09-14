cc2a86511f117e3c1e154e2a54f575e3a171336f
Arturo Polanco Lozano <apolanco3225@gmail.com>
2023-09-14 19:10:40 +0200

    include functions for test normal distribution, skewness and kurtosis

--- Introduction to Portfolio Construction and Analysis with Python/Week 1/stark/metrics.py
+++ Introduction to Portfolio Construction and Analysis with Python/Week 1/stark/metrics.py
@@ -1,5 +1,6 @@
 # quant finance metrics library Arturo Polanco
 import pandas as pd
+import scipy.stats
 
 def drawdown(input_series:pd.Series):
     """
@@ -24,3 +25,47 @@ def drawdown(input_series:pd.Series):
         }
     )
     return output_data
+
+
+def skewness(input_returns):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied series or dataframe
+    Returns a float or a series.
+    """
+    demeaned_returns = input_returns.subtract(input_returns.mean())
+    # use the population standard deviation, so set dof=0
+    sigma_r = input_returns.std(ddof=0)
+    sigma_r_cubed = sigma_r ** 3
+    exp = demeaned_returns.pow(3).mean()
+
+    return exp / sigma_r_cubed
+
+
+def kurtosis(input_returns):
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied series or dataframe
+    Returns a float or a series.
+    """
+    demeaned_returns = input_returns.subtract(input_returns.mean())
+    # use the population standard deviation, so set dof=0
+    sigma_r = input_returns.std(ddof=0)
+    sigma_r_the_fourth = sigma_r ** 4
+    exp = demeaned_returns.pow(4).mean()
+
+    return exp / sigma_r_the_fourth
+
+
+def is_normal(input_returns, level=0.01):
+    """
+    Apply Jarque-Bera test to check
+    for normal distribution returns. 
+    Test is applied at the 1% level by default.
+    Returns True if the hypothesis of normality is accepted, 
+    False otherwise. 
+    """
+
+    statistics, p_value = scipy.stats.jarque_bera(input_returns)
+
+    return p_value > level
